29327ac35135a3493d3d313e765ced43521c7e81
Hoang Nghiem <hoangnghiem111@gmail.com>
2014-06-30 10:45:57 +0700

    Update

--- db/schema.rb
+++ db/schema.rb
@@ -16,12 +16,6 @@ ActiveRecord::Schema.define(version: 20140606162018) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "products", force: true do |t|
-    t.string   "name"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
   create_table "tenant_memberships", force: true do |t|
     t.integer  "tenant_id"
     t.integer  "user_id"
